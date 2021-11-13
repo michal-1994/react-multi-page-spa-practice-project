68f951de9c1537827ce71844abb11adaf9b742c1
Michal <michalgrzegorczyk04@gmail.com>
2021-11-13 13:02:32 +0100

    Add http request

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,19 +1,26 @@
+import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 const NewQuote = () => {
+    const { sendRequest, status } = useHttp(addQuote);
     const history = useHistory();
 
-    const addQuoteHandler = quoteData => {
-        console.log(quoteData, history);
+    useEffect(() => {
+        if (status === 'complited') {
+            history.push('/quotes');
+        }
+    }, [status, history]);
 
-        history.push('/quotes');
-        console.log(history);
+    const addQuoteHandler = quoteData => {
+        sendRequest(quoteData);
     }
 
     return (
-        <QuoteForm onAddQuote={addQuoteHandler} />
+        <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />
     );
 }
 
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,36 +1,37 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
-
-const DUMMY_QUOTES = [
-    {
-        id: 'q1',
-        author: 'Michael',
-        text: 'Learning React is fun!',
-    },
-    {
-        id: 'q2',
-        author: 'Max',
-        text: 'Learning React is great!',
-    },
-];
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 
 const QuoteDetail = () => {
     const match = useRouteMatch();
-
     const params = useParams();
+    const { quoteId } = params;
+    const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
 
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId])
 
-    if (!quote) {
-        return <p>No quote found.</p>;
+    if (status === 'pending') {
+        return <div className="centered"><LoadingSpinner /></div>;
+    }
+
+    if (error) {
+        return <p className="centered">{error}</p>;
+    }
+
+    if (!loadedQuote.text) {
+        return <p className="centered">No quote found.</p>;
     }
 
     return (
         <Fragment>
-            <HighlightedQuote text={quote.text} author={quote.author} />
+            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
             <Route path={`${match.path}`} exact>
                 <div className="centered">
                     <Link className="btn--flat" to={`${match.url}/comments`}>
